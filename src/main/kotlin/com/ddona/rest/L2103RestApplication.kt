ad4a252a009704f6781f59d8bb61575b780ddd09
DoanPT1 <doanpt93cntt@gmail.com>
2022-02-15 20:13:12 +0700

    init empty spring boot project

---
+++ src/main/kotlin/com/ddona/rest/L2103RestApplication.kt
@@ -0,0 +1,11 @@
+package com.ddona.rest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class L2103RestApplication
+
+fun main(args: Array<String>) {
+    runApplication<L2103RestApplication>(*args)
+}
---
+++ src/test/kotlin/com/ddona/rest/L2103RestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ddona.rest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class L2103RestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
